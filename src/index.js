a8cb0d385b401b0d918004d4e45e328d7a346ca4
bioputer <109363628+bioputer@users.noreply.github.com>
2022-07-17 19:25:11 +0900

    テスト

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
-<p>テスト</p>
+<p>テスト！９</p>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
